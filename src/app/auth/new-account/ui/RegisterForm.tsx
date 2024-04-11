5ea41dd892ecf3259acc84cf153f313e640eb02d
Scarleth Abigail Baquedano Padilla <scarleth.baquedano@tigo.com.hn>
2024-04-11 14:56:28 -0600

    Fin seccion 20

--- src/app/auth/new-account/ui/RegisterForm.tsx
+++ src/app/auth/new-account/ui/RegisterForm.tsx
@@ -1,96 +1,112 @@
 'use client';
 
+import { login, registerUser } from "@/actions";
 import clsx from "clsx";
 import Link from "next/link";
+import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 type FormInputs = {
-    name: string;
-    email: string;
-    password: string;
-  }
-  
+  name: string;
+  email: string;
+  password: string;
+}
+
 
 export const RegisterForm = () => {
 
-    const {register, handleSubmit, formState: {errors}} = useForm<FormInputs>();
+  const [errorMessage, setErrorMessage] = useState('')
 
-    const onSubmit: SubmitHandler<FormInputs> = async(data) => {
-        const {name, email, password} = data;
+  const { register, handleSubmit, formState: { errors } } = useForm<FormInputs>();
+
+  const onSubmit: SubmitHandler<FormInputs> = async (data) => {
+
+    setErrorMessage('');
+
+    const { name, email, password } = data;
+
+    //Server action
+    const resp = await registerUser(name, email, password);
+
+    if (!resp.ok) {
+
+      setErrorMessage(resp.message);
+      return;
     }
+
+    await login (email.toLowerCase(), password);
+    window.location.replace('/');
+    
+  }
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
 
-        {/* {
-            errors.name?.type === 'required' && (
-                <span className="text-red-500">*El nombre es obligatorio</span>
-            )
-        } */}
-
       <label htmlFor="email">Nombre completo</label>
-        <input
-           className={
-            clsx(
-              "px-5 py-2 border bg-gray-200 rounded mb-5",
-              {
-                'border-red-500': errors.name
-              }
-            )
-          }
-          type="text"
-          autoFocus
-          { ...register('name', { required: true }) }
-          />
+      <input
+        className={
+          clsx(
+            "px-5 py-2 border bg-gray-200 rounded mb-5",
+            {
+              'border-red-500': errors.name
+            }
+          )
+        }
+        type="text"
+        autoFocus
+        {...register('name', { required: true })}
+      />
 
-        <label htmlFor="email">Correo electrónico</label>
-        <input
-          className={
-            clsx(
-              "px-5 py-2 border bg-gray-200 rounded mb-5",
-              {
-                'border-red-500': errors.email
-              }
-            )
-          }
-          {...register('email', {required: true, pattern: /\S+@\S+\.\S+/})}
-          />
-
-
-        <label htmlFor="email">Contraseña</label>
-        <input
-          className={
-            clsx(
-              "px-5 py-2 border bg-gray-200 rounded mb-5",
-              {
-                'border-red-500': errors.password
-              }
-            )
-          }
-          type="password" 
-          {...register('password', {required: true, minLength: 6})}
-          />
-
-        <button
-          
-          className="btn-primary">
-          Crear cuenta
-        </button>
+      <label htmlFor="email">Correo electrónico</label>
+      <input
+        className={
+          clsx(
+            "px-5 py-2 border bg-gray-200 rounded mb-5",
+            {
+              'border-red-500': errors.email
+            }
+          )
+        }
+        {...register('email', { required: true, pattern: /\S+@\S+\.\S+/ })}
+      />
 
 
-        {/* divisor l ine */ }
-        <div className="flex items-center my-5">
-          <div className="flex-1 border-t border-gray-500"></div>
-          <div className="px-2 text-gray-800">O</div>
-          <div className="flex-1 border-t border-gray-500"></div>
-        </div>
+      <label htmlFor="email">Contraseña</label>
+      <input
+        className={
+          clsx(
+            "px-5 py-2 border bg-gray-200 rounded mb-5",
+            {
+              'border-red-500': errors.password
+            }
+          )
+        }
+        type="password"
+        {...register('password', { required: true, minLength: 6 })}
+      />
 
-        <Link
-          href="/auth/login" 
-          className="btn-secondary text-center">
-          Ingresar
-        </Link>
+      <span className="text-red-500">{errorMessage}</span>
 
-      </form>
+      <button
+
+        className="btn-primary">
+        Crear cuenta
+      </button>
+
+
+      {/* divisor l ine */}
+      <div className="flex items-center my-5">
+        <div className="flex-1 border-t border-gray-500"></div>
+        <div className="px-2 text-gray-800">O</div>
+        <div className="flex-1 border-t border-gray-500"></div>
+      </div>
+
+      <Link
+        href="/auth/login"
+        className="btn-secondary text-center">
+        Ingresar
+      </Link>
+
+    </form>
   )
 }
--- src/auth.config.ts
+++ src/auth.config.ts
@@ -39,8 +39,6 @@ export const authConfig: NextAuthConfig = {
           return session;
         },
     
-    
-    
       },
     
 
